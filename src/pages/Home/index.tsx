83bd90190c38e3f7dad8f6312e2cf0c086c1aa26
Matheus Padilha <mvpadilha94@gmail.com>
2022-03-17 19:41:51 -0300

    Feat: Removendo codigo nao usado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,11 +23,11 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    return {}
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -45,7 +45,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    console.log('item id: ' + id);
   }
 
   return (
@@ -62,30 +62,13 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
           </button>
         </li>
       ))}
-      {/*<li>*/}
-      {/*  <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />*/}
-      {/*  <strong>Tênis de Caminhada Leve Confortável</strong>*/}
-      {/*  <span>R$ 179,90</span>*/}
-      {/*  <button*/}
-      {/*    type="button"*/}
-      {/*    data-testid="add-product-button"*/}
-      {/*  // onClick={() => handleAddProduct(product.id)}*/}
-      {/*  >*/}
-      {/*    <div data-testid="cart-product-quantity">*/}
-      {/*      <MdAddShoppingCart size={16} color="#FFF" />*/}
-      {/*      /!* {cartItemsAmount[product.id] || 0} *!/ 2*/}
-      {/*    </div>*/}
-
-      {/*    <span>ADICIONAR AO CARRINHO</span>*/}
-      {/*  </button>*/}
-      {/*</li>*/}
     </ProductList>
   );
 };
